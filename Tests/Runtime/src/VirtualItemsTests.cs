c5e639b4712701be24d834e5f61e433c9c2cabd0
Konstantin Gindemit <konstantin.gindemit@gmail.com>
2023-01-05 10:48:18 +0100

    New package version for io.getready.rgn.virtualitems, version: 0.0.14 internal revision 2

--- Tests/Runtime/src/VirtualItemsTests.cs
+++ Tests/Runtime/src/VirtualItemsTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
@@ -6,6 +7,7 @@ using RGN.Modules.VirtualItems;
 using RGN.Tests;
 using UnityEngine;
 using UnityEngine.TestTools;
+using static UnityEngine.Networking.UnityWebRequest;
 
 namespace RGN.VirtualItems.Tests.Runtime
 {
@@ -25,12 +27,13 @@ namespace RGN.VirtualItems.Tests.Runtime
                 isStackable = isStackable,
             };
 
-            var task = VirtualItemModule.I.AddVirtualItem(virtualItem);
+            var task = VirtualItemModule.I.AddVirtualItemAsync(virtualItem);
             yield return task.AsIEnumeratorReturnNull();
             var result = task.Result;
 
             Assert.NotNull(result, "The result is null");
             UnityEngine.Debug.Log("Added new virtual item: " + result.id);
+            CheckVirtualItemFields_CreatedUpdated(result);
         }
 
         [UnityTest]
@@ -45,7 +48,7 @@ namespace RGN.VirtualItems.Tests.Runtime
                 isStackable = isStackable,
             };
 
-            var task = VirtualItemModule.I.AddVirtualItem(virtualItem);
+            var task = VirtualItemModule.I.AddVirtualItemAsync(virtualItem);
             yield return task.AsIEnumeratorReturnNullDontThrow();
 
             Assert.True(task.IsFaulted, "Virtual item was added to db even with normal user account. Only admins or creators can add new items");
@@ -62,13 +65,21 @@ namespace RGN.VirtualItems.Tests.Runtime
                 "3ddc10ae-8a04-4f9f-9cf5-fc5b0e6b4cb8"
             };
 
-            var task = VirtualItemModule.I.GetVirtualItemsByIds(ids);
+            var task = VirtualItemModule.I.GetVirtualItemsByIdsAsync(ids);
             yield return task.AsIEnumeratorReturnNull();
             var result = task.Result;
 
             Assert.NotNull(result, "The result is null");
             Assert.IsNotEmpty(result, "Got empty list for existing items");
             Assert.AreEqual(ids.Count, result.Count, "The count of result items does not equal the count of requested items");
+
+            if (result != null)
+            {
+                foreach(VirtualItem item in result)
+                {
+                    CheckVirtualItemFields_CreatedUpdated(item);
+                }
+            }
         }
         [UnityTest]
         public IEnumerator GetVirtualItemsByIds_NoItemsForNonExistingItems()
@@ -81,13 +92,21 @@ namespace RGN.VirtualItems.Tests.Runtime
                 "non_existing_item_two"
             };
 
-            var task = VirtualItemModule.I.GetVirtualItemsByIds(ids);
+            var task = VirtualItemModule.I.GetVirtualItemsByIdsAsync(ids);
             yield return task.AsIEnumeratorReturnNull();
             var result = task.Result;
 
             Assert.NotNull(result, "The result is null");
             Assert.IsEmpty(result, "Got non empty list for non existing items");
             Assert.AreEqual(0, result.Count);
+
+            if (result != null)
+            {
+                foreach (VirtualItem item in result)
+                {
+                    CheckVirtualItemFields_CreatedUpdated(item);
+                }
+            }
         }
         [UnityTest]
         public IEnumerator GetVirtualItemsByIds_ReturnsOnlyExistingItems()
@@ -102,13 +121,21 @@ namespace RGN.VirtualItems.Tests.Runtime
                 "3ddc10ae-8a04-4f9f-9cf5-fc5b0e6b4cb8"
             };
 
-            var task = VirtualItemModule.I.GetVirtualItemsByIds(ids);
+            var task = VirtualItemModule.I.GetVirtualItemsByIdsAsync(ids);
             yield return task.AsIEnumeratorReturnNull();
             var result = task.Result;
 
             Assert.NotNull(result, "The result is null");
             Assert.IsNotEmpty(result, "Got empty list for existing items");
             Assert.AreEqual(2, result.Count);
+
+            if (result != null)
+            {
+                foreach (VirtualItem item in result)
+                {
+                    CheckVirtualItemFields_CreatedUpdated(item);
+                }
+            }
         }
 
         [UnityTest]
@@ -116,12 +143,20 @@ namespace RGN.VirtualItems.Tests.Runtime
         {
             yield return LoginAsNormalTester();
 
-            var task = VirtualItemModule.I.GetVirtualItems();
+            var task = VirtualItemModule.I.GetVirtualItemsAsync();
             yield return task.AsIEnumeratorReturnNull();
             var result = task.Result;
 
             Assert.NotNull(result, "The result is null");
             Assert.IsNotEmpty(result, "Got empty list for current app: " + RGNCoreBuilder.I.AppIDForRequests);
+
+            if (result != null)
+            {
+                foreach (VirtualItem item in result)
+                {
+                    CheckVirtualItemFields_CreatedUpdated(item);
+                }
+            }
         }
 
         [UnityTest]
@@ -135,12 +170,20 @@ namespace RGN.VirtualItems.Tests.Runtime
                 "io.test.test"
             };
 
-            var task = VirtualItemModule.I.GetAllVirtualItemsByAppIds(appIds);
+            var task = VirtualItemModule.I.GetAllVirtualItemsByAppIdsAsync(appIds);
             yield return task.AsIEnumeratorReturnNull();
             var result = task.Result;
 
             Assert.NotNull(result, "The result is null");
             Assert.IsNotEmpty(result, "Got empty list for requested apps");
+
+            if (result != null)
+            {
+                foreach (VirtualItem item in result)
+                {
+                    CheckVirtualItemFields_CreatedUpdated(item);
+                }
+            }
         }
 
         [UnityTest]
@@ -153,7 +196,7 @@ namespace RGN.VirtualItems.Tests.Runtime
 
             var virtualItemModule = VirtualItemModule.I;
 
-            var getVirtualItemTagsTask = virtualItemModule.GetTags(virtualItemId);
+            var getVirtualItemTagsTask = virtualItemModule.GetTagsAsync(virtualItemId);
             yield return getVirtualItemTagsTask.AsIEnumeratorReturnNull();
             var getVirtualItemTagsResult = getVirtualItemTagsTask.Result;
 
@@ -174,10 +217,10 @@ namespace RGN.VirtualItems.Tests.Runtime
 
             var virtualItemModule = VirtualItemModule.I;
 
-            var setTagsTask = virtualItemModule.SetTags(virtualItemId, newTags);
+            var setTagsTask = virtualItemModule.SetTagsAsync(virtualItemId, newTags);
             yield return setTagsTask.AsIEnumeratorReturnNull();
 
-            var getVirtualItemTagsTask = virtualItemModule.GetTags(virtualItemId);
+            var getVirtualItemTagsTask = virtualItemModule.GetTagsAsync(virtualItemId);
             yield return getVirtualItemTagsTask.AsIEnumeratorReturnNull();
             var getVirtualItemTagsResult = getVirtualItemTagsTask.Result;
 
@@ -208,10 +251,10 @@ namespace RGN.VirtualItems.Tests.Runtime
 
             var virtualItemModule = VirtualItemModule.I;
 
-            var setNameTask = virtualItemModule.SetName(virtualItemId, newName);
+            var setNameTask = virtualItemModule.SetNameAsync(virtualItemId, newName);
             yield return setNameTask.AsIEnumeratorReturnNull();
 
-            var getVirtualItemsTask = virtualItemModule.GetVirtualItemsByIds(new List<string> { virtualItemId });
+            var getVirtualItemsTask = virtualItemModule.GetVirtualItemsByIdsAsync(new List<string> { virtualItemId });
             yield return getVirtualItemsTask.AsIEnumeratorReturnNull();
             var getVirtualItemsResult = getVirtualItemsTask.Result;
 
@@ -231,10 +274,10 @@ namespace RGN.VirtualItems.Tests.Runtime
 
             var virtualItemModule = VirtualItemModule.I;
 
-            var setDescriptionTask = virtualItemModule.SetDescription(virtualItemId, newDescription);
+            var setDescriptionTask = virtualItemModule.SetDescriptionAsync(virtualItemId, newDescription);
             yield return setDescriptionTask.AsIEnumeratorReturnNull();
 
-            var getVirtualItemsTask = virtualItemModule.GetVirtualItemsByIds(new List<string> { virtualItemId });
+            var getVirtualItemsTask = virtualItemModule.GetVirtualItemsByIdsAsync(new List<string> { virtualItemId });
             yield return getVirtualItemsTask.AsIEnumeratorReturnNull();
             var getVirtualItemsResult = getVirtualItemsTask.Result;
 
@@ -251,7 +294,7 @@ namespace RGN.VirtualItems.Tests.Runtime
             var virtualItemId = "92c7067d-cb58-4f3d-a545-36faf409d64c";
             var propertiesToSet = "{}";
 
-            var task = VirtualItemModule.I.SetProperties(virtualItemId, propertiesToSet);
+            var task = VirtualItemModule.I.SetPropertiesAsync(virtualItemId, propertiesToSet);
             yield return task.AsIEnumeratorReturnNull();
             var result = task.Result;
 
@@ -268,7 +311,7 @@ namespace RGN.VirtualItems.Tests.Runtime
             var virtualItemId = "92c7067d-cb58-4f3d-a545-36faf409d64c";
             var expectedProperties = "{}";
 
-            var task = VirtualItemModule.I.GetProperties(virtualItemId);
+            var task = VirtualItemModule.I.GetPropertiesAsync(virtualItemId);
             yield return task.AsIEnumeratorReturnNull();
             var result = task.Result;
 
@@ -276,17 +319,51 @@ namespace RGN.VirtualItems.Tests.Runtime
             Assert.AreEqual(expectedProperties, result);
             UnityEngine.Debug.Log(result);
         }
+
         // TODO: [UnityTest]
         public IEnumerator DownloadVirtualItemThumbnail_CastToUnityTexture2DWorks()
         {
             yield return LoginAsNormalTester();
 
             var virtualItemId = "92c7067d-cb58-4f3d-a545-36faf409d64c";
-            var task = VirtualItemModule.I.DownloadVirtualItemThumbnail<Texture2D>(virtualItemId);
+            var task = VirtualItemModule.I.DownloadVirtualItemThumbnailAsync<Texture2D>(virtualItemId);
             yield return task.AsIEnumeratorReturnNull();
 
             Texture2D result = task.Result;
             Debug.Log(result.width + ":" + result.height);
         }
+
+        // helper function for checking of VirtualItems fields: createdAt, updatedAt, createdBy, updatedBy
+        private void CheckVirtualItemFields_CreatedUpdated(VirtualItem item)
+        {
+            if (item == null)
+                return;
+
+            if (!string.IsNullOrEmpty(item.createdAt))
+            {
+                DateTime dt;
+                bool result = DateTime.TryParse(item.createdAt, out dt);
+                Assert.IsTrue(result, "createdAt has wrong format for Item " + item.id);
+            }
+            else
+            {
+                Assert.Fail("createdAt is Null for Item " + item.id);
+            }
+
+            if (!string.IsNullOrEmpty(item.updatedAt))
+            {
+                DateTime dt;
+                bool result = DateTime.TryParse(item.updatedAt, out dt);
+                Assert.IsTrue(result, "updatedAt has wrong format for Item " + item.id);
+            }
+            else
+            {
+                Assert.Fail("updatedAt is Null for Item " + item.id);
+            }
+
+            Assert.IsFalse(string.IsNullOrEmpty(item.createdBy), "createdBy is Null or Empty for Item " + item.id);
+            Assert.IsFalse(string.IsNullOrEmpty(item.updatedBy), "updatedBy is Null or Empty for Item " + item.id);
+        }
+
     }
 }
